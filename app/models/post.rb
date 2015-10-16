f59abe112bf2fa3e771fd28785f1c27b7b18845c
AlexAlt <alexandra.m.altieri@gmail.com>
2015-10-16 10:35:03 -0700

    Pass spec that tests default scope for ordering Posts in descending order by creation date/time

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
 
   has_and_belongs_to_many :tags
 
+  default_scope {order('created_at DESC')}
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,5 +6,13 @@ describe Post do
   it {should validate_presence_of :content }
 
   it {should have_and_belong_to_many :tags}
+
+  describe 'default scope' do
+  it 'orders by descending name' do
+    post_one = Post.create(title: "First Post!", user_name: "Me", content: "This is my first post!")
+    post_two = Post.create(title: "Second Post!", user_name: "Me", content: "This is my second post!")
+    expect(Post.all).to eq [post_two, post_one]
+  end
+end
   
 end
