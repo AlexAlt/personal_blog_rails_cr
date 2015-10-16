8c2b105343aac60e995724b265066a91249dda50
AlexAlt <alexandra.m.altieri@gmail.com>
2015-10-16 15:48:41 -0700

    Pass spec for updating Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,10 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
@@ -17,8 +21,18 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
+  def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+      flash[:notice] = "Success!"
+    else
+      render :edit
+    end
   end
 
   private
---
+++ spec/features/edit_post_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "the edit a post process" do
+  it "edits a post" do
+    new_post = Post.create({title: "Hi", user_name: "Alex", content: "Hi"})
+    visit edit_post_path(new_post)
+    fill_in 'Title', :with => 'Hello'
+    click_on 'Update Post'
+    expect(page).to have_content "Success!"
+  end
+
+end
