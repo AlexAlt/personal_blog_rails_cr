57d0416fc93f34075c55b88fd80b85d7f6847ecf
AlexAlt <alexandra.m.altieri@gmail.com>
2015-10-24 13:50:28 -0700

    Edit about image

--- spec/features/show_post_pages_spec.rb
+++ spec/features/show_post_pages_spec.rb
@@ -1,10 +1,10 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "the show a post process" do
-#   it "shows an individual post" do
-#     new_post = Post.create({title: "Hi", user_name: "Alex", content: "Hi"})
-#     visit posts_path
-#     click_link 'Hi'
-#     expect(page).to have_content "Hi"
-#   end
-# end
+describe "the show a post process" do
+  it "shows an individual post" do
+    new_post = Post.create({title: "Hi", content: "Hi"})
+    visit user_posts_path
+    click_link 'Hi'
+    expect(page).to have_content "Hi"
+  end
+end
