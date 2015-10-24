aa2224a523984ae9359d4be586f86f34229dbb7a
AlexAlt <alexandra.m.altieri@gmail.com>
2015-10-24 13:29:51 -0700

    Add more spec coverage for editing posts

--- spec/features/edit_post_pages_spec.rb
+++ spec/features/edit_post_pages_spec.rb
@@ -1,12 +1,24 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "the edit a post process" do
-#   it "edits a post" do
-#     new_post = Post.create({title: "Hi", user_name: "Alex", content: "Hi"})
-#     visit edit_post_path(new_post)
-#     fill_in 'Title', :with => 'Hello'
-#     click_on 'Update Post'
-#     expect(page).to have_content "Success!"
-#   end
+describe "the edit a post process" do
+    before :each do
+    @admin = User.new({username: "Alex", admin: true, password: "password"})
+    @admin.save
+    visit '/'
+    click_on "Log In"
+    fill_in 'Username', :with => "Alex"
+    fill_in 'Password', :with => "password"
+    within("#log-in") do
+      click_on("Log In")
+    end
+  end
 
-# end
+  it "edits a post" do
+    new_post = Post.create({title: "Hi", content: "Hi", user_id: @admin.id})
+    visit edit_user_post_path(@admin, new_post)
+    fill_in 'Title', :with => 'Hello'
+    click_on 'Update Post'
+    expect(page).to have_content "Success!"
+  end
+
+end
